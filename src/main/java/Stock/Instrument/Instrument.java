e78c0dadeb2109ec81e0b770b2a3f0d84c59dfee
1anchen <svgyc@hotmail.co.uk>
2018-05-25 17:57:46 +0100

    created enum playtype

--- src/main/java/Stock/Instrument/Instrument.java
+++ src/main/java/Stock/Instrument/Instrument.java
@@ -1,4 +1,18 @@
 package Stock.Instrument;
 
-public class Instrument {
+import Stock.Stock;
+
+public class Instrument extends Stock {
+
+    protected String material;
+    protected PlayType playType;
+
+    public Instrument(String name, String manufacturer, String color, int wholesellPrice,
+                      int retailPrice, int stockCount, String material, PlayType playtype) {
+
+        super(name,manufacturer,color,wholesellPrice,retailPrice,stockCount);
+        this.material =material;
+        this.playType =playType;
+    }
+
 }
---
+++ src/main/java/Stock/Instrument/PlayType.java
@@ -0,0 +1,4 @@
+package Stock.Instrument;
+
+public enum PlayType {
+}
