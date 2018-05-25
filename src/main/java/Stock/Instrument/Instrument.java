63081d81a19a554286469322f38d50cc1b2bb29a
1anchen <svgyc@hotmail.co.uk>
2018-05-25 15:36:20 +0100

    starting the Insturment class

---
+++ src/main/java/Stock/Instrument/Instrument.java
@@ -0,0 +1,4 @@
+package Stock.Instrument;
+
+public class Instrument {
+}
--- src/main/java/Stock/Stock.java
+++ src/main/java/Stock/Stock.java
@@ -1,4 +1,47 @@
 package Stock;
 
-public class Stock {
+public abstract class Stock {
+    protected String name;
+    protected String manufacturer;
+    protected String color;
+    protected int wholesellPrice;
+    protected int retailPrice;
+    protected int stockCount;
+
+    public Stock(String name, String manufacturer, String color, int wholesellPrice, int retailPrice, int stockCount){
+        this.name =name;
+        this.manufacturer =manufacturer;
+        this.color =color;
+        this.wholesellPrice =wholesellPrice;
+        this.retailPrice =retailPrice;
+        this.stockCount =stockCount;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getManufacturer(){
+        return this.manufacturer;
+    }
+
+    public String getColor(){
+        return this.manufacturer;
+    }
+
+    public int getWholesellPrice(){
+        return this.wholesellPrice;
+    }
+
+    public int getRetailPrice(){
+        return this.retailPrice;
+    }
+
+    public int getStockCount(){
+        return this.stockCount;
+    }
+
+
 }
+
+
